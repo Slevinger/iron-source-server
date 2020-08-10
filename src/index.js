82e5eb60578b891f31cd4605b98c6bed72b2368a
slevinger <shir.levinger@gmail.com>
2020-08-10 03:33:08 +0300

    PORT

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const surpriseRoutes = require("./module/surprise/surprise.routes");
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 let server;
 
 (async () => {
